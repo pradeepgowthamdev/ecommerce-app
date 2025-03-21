b952878382dcd0d2436ffdde7e11507bbe2ca5a7
Pradeep Gowtham <pradeepgowthamdev@gmail.com>
2025-03-21 15:47:43 -0500

    test file name missmatched

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import NewsLetterBox from '../components/NewsLetterBox'
+import NewsLetterBox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
